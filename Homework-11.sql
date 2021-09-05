32555051d6a423fb9f72c4bce6c2e50050f99f1c
Dogancan Ulgu <dogancanulgu@gmail.com>
2021-09-05 03:30:01 +0300

    HW 11 is completed

---
+++ Homework-11.sql
@@ -0,0 +1,31 @@
+-- actor ve customer tablolarında bulunan first_name sütunları için tüm verileri sıralayalım.
+
+(SELECT first_name from actor)
+UNION
+(SELECT first_name from customer);
+
+-- actor ve customer tablolarında bulunan first_name sütunları için kesişen verileri sıralayalım.
+
+(SELECT first_name from actor)
+INTERSECT
+(SELECT first_name from customer);
+
+-- actor ve customer tablolarında bulunan first_name sütunları için ilk tabloda bulunan ancak ikinci tabloda bulunmayan verileri sıralayalım.
+
+(SELECT first_name from actor)
+EXCEPT
+(SELECT first_name from customer);
+
+-- İlk 3 sorguyu tekrar eden veriler için de yapalım.
+
+(SELECT first_name from actor)
+UNION ALL
+(SELECT first_name from customer);
+
+(SELECT first_name from actor)
+INTERSECT ALL
+(SELECT first_name from customer);
+
+(SELECT first_name from actor)
+EXCEPT ALL
+(SELECT first_name from customer);
